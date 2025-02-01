2bed9872c26e24fa0f97004543ed13456eed51d4
VIGNESH BABU T R <110011481+VICKY-0017@users.noreply.github.com>
2025-02-02 00:29:04 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: "http://localhost:3000",
+    origin: "http://localhost:3000","https://crm-frntd.onrender.com",
     methods: "GET,HEAD,PUT,PATCH,POST,DELETE",
     credentials: true,
   })
